bb2871b11b4334c95da45b5a849b83a7a3fd9036
Anton Coleridge <43687643+AntonColeridge@users.noreply.github.com>
2018-09-29 06:37:04 -0400

    FORMAT:
    Created a toolbox file to store all fucntion definitions
    to keep mainfiles short and clean. Also made renamed subscriptions "table"
    to subscriber as it is more appropriate

--- api/subscriptions/controller.js
+++ api/subscriptions/controller.js
@@ -1,39 +1,16 @@
 //SETUP========================================================================
+const mockDataLocation = '../../utility/mock-data';
+const toolboxLocation = '../../utility/toolbox';
 
-    const {categorySubscriptions,categories}= require('../../utility/mock-data');
+const { categorySubscibers, categories } = require(mockDataLocation);
+const { numberOfSubscibers } = require(toolboxLocation);
 
 //CONFIGURATION================================================================
-//for each category object store the name[x]
-//for each category object store the id[X]
-//for each category object get the number of subscibers 
-//that match the id (filter>count new array to get#)[x]
-const numberOfSubscibers = (categorySubscriptions, categories) => {
-    const results = [];
-    let name, id, subCount, listOfSubs;
 
-    categories.forEach(element => {
-        name = element.name;
-        id = element.id;
-        listOfSubs = categorySubscriptions.filter(element => {
-            return element.categoryId == id;
-        })
 
-        //build object with name, id,subsciber count and push to results array[x]
-        subCount = listOfSubs.length;
-        results.push({
-            name,
-            subCount
-
-        })
-
-    });
-
-    return results;
-
-}
 
 const GET_numberOfSubscibers = (req, res) => {
-    res.json(numberOfSubscibers(categorySubscriptions,categories))
+    res.json(numberOfSubscibers(categorySubscibers, categories))
 
 }
 const POST_unsub = (req, res) => { }
@@ -41,7 +18,7 @@ const POST_sub = (req, res) => { }
 
 module.exports = {
     GET_numberOfSubscibers,
-    POST_sub,                                                
+    POST_sub,
     POST_unsub
 
 }
